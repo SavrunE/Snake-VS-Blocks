a390efaa2a8880aceb0902a7cfcdd5d5df8cf7f6
SavrunE <igor1996igor@inbox.ru>
2020-12-25 05:57:38 +0300

    base rdy

--- Snake VS blocks/Assets/Scripts/Spawner/Spawner.cs
+++ Snake VS blocks/Assets/Scripts/Spawner/Spawner.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [Header("Settings")]
     [SerializeField] private Transform container;
     [SerializeField] private int repeatCount;
     [SerializeField] private int distanceBetweenFullLine;
     [SerializeField] private int distanceBetweenRandomLine;
 
+    [Header("Blocks")]
     [SerializeField] private Block blockTemplate;
     [SerializeField] private int blockSpawnChance;
 
-    private SpawnPoint[] blocksSpawnPoints;
+    [Header("Walls")]
+    [SerializeField] private Wall wallTemplate;
+    [SerializeField] private int wallSpawnChance;
+
+    private BlockSpawnPoint[] blocksSpawnPoints;
+    private WallSpawnPoint[] wallSpawnPoints;
     private void Start()
     {
-        blocksSpawnPoints = GetComponentsInChildren<SpawnPoint>();
+        blocksSpawnPoints = GetComponentsInChildren<BlockSpawnPoint>();
+        wallSpawnPoints = GetComponentsInChildren<WallSpawnPoint>();
 
         for (int i = 0; i < repeatCount; i++)
         {
+            MoveSpawner(distanceBetweenFullLine);
+            GenerateRandomLine(wallSpawnPoints, wallTemplate.gameObject, wallSpawnChance, distanceBetweenFullLine, distanceBetweenFullLine / 2f);
             GenerateFullLine(blocksSpawnPoints, blockTemplate.gameObject);
             MoveSpawner(distanceBetweenFullLine);
+            GenerateRandomLine(wallSpawnPoints, wallTemplate.gameObject, wallSpawnChance, distanceBetweenRandomLine, distanceBetweenRandomLine / 2f);
+            GenerateRandomLine(blocksSpawnPoints, blockTemplate.gameObject, blockSpawnChance);
         }
     }
 
@@ -32,17 +44,34 @@ public class Spawner : MonoBehaviour
         }
     }
 
-    private void GenerateRandomLine()
+    private void GenerateRandomLine(SpawnPoint[] spawnPoints, GameObject generatedElement, int spawnChance)
     {
-
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (Random.Range(0, 100) < spawnChance)
+            {
+                GameObject element = GenerateElement(spawnPoints[i].transform.position, generatedElement);
+            }
+        }
+    }
+    private void GenerateRandomLine(SpawnPoint[] spawnPoints, GameObject generatedElement, int spawnChance, int scaleY = 1, float offsetY = 0)
+    {
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (Random.Range(0, 100) < spawnChance)
+            {
+                GameObject element = GenerateElement(spawnPoints[i].transform.position, generatedElement, offsetY);
+                element.transform.localScale = new Vector3(element.transform.localScale.x, scaleY, element.transform.localScale.z);
+            }
+        }
     }
 
-    private GameObject GenerateElement(Vector3 spawnPoint,GameObject generatedElement)
+    private GameObject GenerateElement(Vector3 spawnPoint, GameObject generatedElement, float offsetY = 0)
     {
-        spawnPoint.y -= generatedElement.transform.localScale.y;
+        spawnPoint.y -= offsetY;
         return Instantiate(generatedElement, spawnPoint, Quaternion.identity, container);
     }
-    
+
     private void MoveSpawner(int distanceY)
     {
         transform.position = new Vector3(transform.position.x, transform.position.y + distanceY, transform.position.z);
